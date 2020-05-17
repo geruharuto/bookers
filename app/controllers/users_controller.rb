aea41c069989e9f9f49700c068f517fc82a242dd
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-17 17:09:29 +0900

    左側ページ形だけ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,21 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id]) #ユーザーを探す
   	@books = @user.books#上のユーザーの持ってる本を全部出す
+    @book = Book.new
   end
   def edit
   	@user = User.find(params[:id])
   end
   def update
+  	@user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 end
 
+private
+def user_params
+   params.require(:user).permit(:name, :introduction)
+end
 #where　指定したものを探してくる　要検索！
 #モデルのカラム：一致させたいデータ
