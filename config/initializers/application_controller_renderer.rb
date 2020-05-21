bf9dc2111a0369d2f4219dd2f478fceed060c326
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-21 15:39:40 +0900

    画像リサイズ、Userのshow、Bookのshow完成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b4f26d960655335611face0b4147c065f84edaf82ce4d33ec33d6ff275d7e639753f356f77f46a8f1773593b930f374b5bd917a4372d13aca216e780379fbe92'
